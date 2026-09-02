2955ba12879f31d2fafbdeb8c3c9c2165ce6168d
mari4nne <165202170+mari4nne@users.noreply.github.com>
2026-09-02 10:16:12 -0300

    Aquario.java

--- src/business/Aquario.java
+++ src/business/Aquario.java
@@ -1,26 +1,49 @@
 package business;
 public class Aquario {
 
-	private static int geraId = 1;
-	private int codigo;
-	private int id;
-	private float volume;
+    private static int geraId = 0;
+    private int codigo;
+    private int id;
+    private float volume;
     private TipoAgua tipo;
-    
-    public Aquario(Aquario a) {
+    public Aquario(){
+        this.id = geraId++;
+    }
+    public Aquario(Aquario a){
         this.id = a.id;
         this.codigo = a.codigo;
         this.volume = a.volume;
         this.tipo = a.tipo;
     }
-
-//    private Usuario(String nome, String nomeUsuario, String senha, TipoUsuario tipo) {
-//        this.id = geraId++;
-//        this.nome = nome;
-//        this.nomeUsuario = nomeUsuario;
-//        this.senha = senha;
-//        this.tipo = tipo;
-//    }
-    
-    
+    public static int getGeraId(){
+        return geraId;
+    }
+    public static void setGeraId(int geraId){
+        Aquario.geraId = geraId;
+	}
+    public int getCodigo(){
+        return codigo;
+    }
+    public void setCodigo(int codigo){
+        this.codigo = codigo;
+    }
+    public int getId(){
+        return id;
+    }
+    public void setId(int id){
+        this.id = id;
+    }
+    public float getVolume(){
+        return volume;
+    }
+    public void setVolume(float volume){
+        this.volume = volume;
+    }
+    public TipoAgua getTipo(){
+        return tipo;
+    }
+    public void setTipo(TipoAgua tipo){
+        this.tipo = tipo;
+    }
 }
+
