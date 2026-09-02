a4abdd7d0d7f81fb70e097c2952cd90f55b930ba
mari4nne <165202170+mari4nne@users.noreply.github.com>
2026-09-02 10:17:58 -0300

    AquarioController.java

--- src/business/AquarioController.java
+++ src/business/AquarioController.java
@@ -1,33 +1,34 @@
 package business;
-
+import java.util.List;
 import repository.AquarioRepository;
 
-import java.util.List;
-
 public class AquarioController {
-    private final AquarioRepository repoAquario;
 
+    private final AquarioRepository repoAquario;
     public AquarioController(){
         repoAquario = new AquarioRepository();
     }
-
     public boolean add(Aquario aquario){
+        if(aquario == null)
             return false;
+        return repoAquario.add(aquario);
     }
-
     public boolean update(Aquario aquarioAlterado){
+        if(aquarioAlterado == null)
             return false;
+        return repoAquario.update(aquarioAlterado);
     }
-
     public Aquario deleteById(int id){
+        if(id < 0)
             return null;
+        return repoAquario.deleteById(id);
     }
-
     public Aquario getById(int id){
+        if(id < 0)
             return null;
+        return repoAquario.getById(id);
     }
-
     public List<Aquario> getAll(){
-        return null;
+        return repoAquario.getAll();
     }
 }
