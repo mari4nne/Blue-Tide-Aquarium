20fdbe80218504e4659ac03a003ecc8e122567e3
mari4nne <165202170+mari4nne@users.noreply.github.com>
2026-09-02 10:20:10 -0300

    AquarioRepository.java

--- src/repository/AquarioRepository.java
+++ src/repository/AquarioRepository.java
@@ -1,34 +1,47 @@
 package repository;
-
-import business.Aquario;
-
 import java.util.ArrayList;
 import java.util.List;
+import business.Aquario;
 
 public class AquarioRepository {
-    private List<Aquario> aquarios;
-
+    private final List<Aquario> aquarios;
     public AquarioRepository(){
         aquarios = new ArrayList<>();
     }
-
     public boolean add(Aquario aquario){
-        return false;
+        if(aquario == null)
+            return false;
+        aquarios.add(aquario);
+        return true;
     }
-
     public boolean update(Aquario aquarioAlterado){
+        if(aquarioAlterado == null)
+            return false;
+        for(int i = 0; i < aquarios.size(); i++){
+            if(aquarios.get(i).getId() == aquarioAlterado.getId()){
+                aquarios.set(i, aquarioAlterado);
+                return true;
+            }
+        }
         return false;
     }
-
     public Aquario deleteById(int id){
+        for(int i = 0; i < aquarios.size(); i++){
+            if(aquarios.get(i).getId() == id){
+                return aquarios.remove(i);
+            }
+        }
         return null;
     }
-
     public Aquario getById(int id){
+        for(Aquario aquario : aquarios){
+            if(aquario.getId() == id){
+                return aquario;
+            }
+        }
         return null;
     }
-
     public List<Aquario> getAll(){
-        return null;
+        return aquarios;
     }
 }
